cd31c9e6e2ad99fe66ffd76acb0005d5138c0f62
Nate Gay <email@nategay.me>
2026-07-20 00:22:17 -0500

    Revert "Use relative install DESTINATION and fprime_install wrapper (#49)"
    This reverts commit de5b2c8bd02fd7af3d71801d145ed58a09467dd9.

--- cmake/toolchain/zephyr.cmake
+++ cmake/toolchain/zephyr.cmake
@@ -69,16 +69,14 @@ function(register_fprime_zephyr_deployment)
     install(
         DIRECTORY ${CMAKE_BINARY_DIR}/zephyr/
         COMPONENT fprime-zephyr-binaries
-        DESTINATION .
+        DESTINATION ${CMAKE_INSTALL_PREFIX}
         FILES_MATCHING PATTERN "zephyr/zephyr.*"
         REGEX "zephyr/[^z]" EXCLUDE
     )
     add_custom_target(${BUILD_TARGET_NAME} ALL DEPENDS zephyr_final fprime-zephyr-deployment_dictionary
         COMMAND "${CMAKE_COMMAND}"
-            -DCMAKE_INSTALL_COMPONENT=fprime-zephyr-binaries
-            -DFPRIME_INSTALL_DIRECTORY=${FPRIME_INSTALL_DIRECTORY}
-            -DFPRIME_BUILD_DIR=${CMAKE_BINARY_DIR}
-            -P ${FPRIME_FRAMEWORK_PATH}/cmake/target/fprime_install.cmake
+            -DCMAKE_INSTALL_COMPONENT=fprime-zephyr-binaries -P ${CMAKE_BINARY_DIR}/cmake_install.cmake
+    
     )
     # Integration with fprime-util is now done via special methods that write-out special metadata. Since we've
     # defined a new way of registering deployments, we must also replicate the target integration metadata.
